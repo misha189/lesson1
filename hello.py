a422cc37706d4c624e18f941e1eacebfd6188e44
Misha <mikhail54n@gmail.com>
2019-09-10 00:04:26 +0700

    Первая версия

---
+++ hello.py
@@ -0,0 +1,13 @@
+def discounted(price, discount):
+    price = abs(float(price))
+    discount = abs(float(discount))
+    if discount >= 100:
+        price_with_discount = price
+    else:
+        price_with_discount = price - price * discount / 100
+    return(price_with_discount)
+
+product = {'name': 'Samsung Galaxy S10', 'stock': 8, 'price': 50000.0, 'discount': 50}
+product['with_discount'] = discounted(product['price'], product['discount'])
+
+print(product)
